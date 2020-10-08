2f9ed2368b6f0333dbea7a44e5f7ecd2fd01d6c6
avosa <websterb17@gmail.com>
2020-10-08 03:18:09 +0300

    First coit

---
+++ src/private/firebase-key.ts
@@ -0,0 +1,8 @@
+export const fireBaseKey = {
+    apiKey: "AIzaSyA1agx7ZE04kVuxHHqEs0DeS3agS21HcjE",
+    authDomain: "ogla1-91ffa.firebaseapp.com",
+    databaseURL: "https://ogla1-91ffa.firebaseio.com",
+    projectId: "ogla1-91ffa",
+    storageBucket: "ogla1-91ffa.appspot.com",
+    messagingSenderId: "716347633449"
+};
